e0121b3682b71d8d3758cfff138a902c39c7eab4
anggara.setiawan <aanggaadc@gmail.com>
2024-08-21 13:06:07 +0700

    fix desktop responsive style

--- slices/TopTourSummary/index.tsx
+++ slices/TopTourSummary/index.tsx
@@ -16,11 +16,11 @@ export type TopTourSummaryProps =
 const TopTourSummary = ({ slice }: TopTourSummaryProps): JSX.Element => {
   return (
     <section
-      className="flex flex-col lg:flex-row lg:gap-9"
+      className="flex flex-col lg:flex-row lg:gap-[4.5rem]"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="w-full aspect-[25/22] overflow-hidden lg:aspect-[171/187] lg:w-6/12">
+      <div className="w-full aspect-[25/22] overflow-hidden lg:aspect-[171/187] lg:w-6/12 2xl:aspect-[924/697]">
         <PrismicNextImage
           className="w-full h-full object-cover object-center"
           field={slice.primary.tour_image}
@@ -28,10 +28,12 @@ const TopTourSummary = ({ slice }: TopTourSummaryProps): JSX.Element => {
       </div>
 
       <div className="max-w-[868px] pt-4 pb-6 px-4 lg:w-6/12  lg:pl-0 lg:pr-12 lg:pt-8 lg:pb-[1.125rem]">
-        <div className="flex items-center justify-between mb-8">
+        <div className="flex items-center justify-between mb-8 lg:justify-start lg:gap-5">
           <div className="flex items-center gap-[0.6rem]">
             <CalendarIcon className="w-[18px] h-[18px] fill-[#414141]" />
-            <span className="font-noto-sans text-gray">Trip Year</span>
+            <span className="font-noto-sans text-gray font-semibold">
+              Trip Year
+            </span>
           </div>
 
           <div className="flex items-center rounded border border-solid border-[#2d5a7b]">
@@ -61,14 +63,14 @@ const TopTourSummary = ({ slice }: TopTourSummaryProps): JSX.Element => {
           field={slice.primary.description}
           components={{
             paragraph: ({ children }) => (
-              <p className="font-noto-sans text-gray text-sm leading-tight mb-8 mt-3 lg:text-base lg:my-3">
+              <p className="font-noto-sans text-gray text-sm leading-tight mb-8 mt-3.5 lg:text-base lg:my-3 lg:leading-[170%]">
                 {children}
               </p>
             ),
           }}
         />
 
-        <div className="grid grid-cols-[repeat(2,1fr)] gap-[2rem_2rem] mt-[0.8rem] mb-6 lg:mt-3 lg:mb-6">
+        <div className="grid grid-cols-[repeat(2,1fr)] gap-[2rem_2rem] mt-[0.8rem] mb-6 lg:mt-3.5 lg:mb-6">
           {slice.primary.details.map((item) => (
             <div key={item.title}>
               <div className="flex items-center gap-2 mb-2">
@@ -83,7 +85,7 @@ const TopTourSummary = ({ slice }: TopTourSummaryProps): JSX.Element => {
                 </p>
               </div>
 
-              <p className="text-sm text-gray leading-[150%]">
+              <p className="text-sm text-gray leading-[150%] lg:text-base">
                 {item.description}
               </p>
             </div>
@@ -120,7 +122,7 @@ const TopTourSummary = ({ slice }: TopTourSummaryProps): JSX.Element => {
           <ChevronLeftIcon className="flex-none fill-[#414141]" />
         </PrismicNextLink>
 
-        <div className="border-b-[#d9d9d9] border-b border-solid">
+        <div className="border-b-[#d9d9d9] border-b border-solid lg:border-none">
           <p className="font-noto-sans text-light-gray mb-[19px]">
             <span className="font-semibold text-gray">
               {slice.primary.trip_code_text}:
